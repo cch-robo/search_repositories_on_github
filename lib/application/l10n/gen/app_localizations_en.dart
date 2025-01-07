7868e2b367be544edcf578d2ccac730c3367f45b
cch-robo <robo@cch-lab.com>
2025-01-07 19:06:03 +0900

    生成された国際化対応クラスを新規追加 - app_localizations.dart: 国際化対応機能提供クラス - app_localizations_ja.dart: 日本語メッセージ提供クラス - app_localizations_en.dart: 英語メッセージ提供クラス

---
+++ lib/application/l10n/gen/app_localizations_en.dart
@@ -0,0 +1,12 @@
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get appDescription =>
+      'The Search Repo app uses the GitHub API to search repositories with arbitrary queries.';
+}
---
+++ lib/application/l10n/gen/app_localizations_ja.dart
@@ -0,0 +1,12 @@
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Japanese (`ja`).
+class AppLocalizationsJa extends AppLocalizations {
+  AppLocalizationsJa([String locale = 'ja']) : super(locale);
+
+  @override
+  String get appDescription =>
+      'Search Repo アプリは、GitHub API を使って任意クエリでリポジトリ検索するアプリです。';
+}
