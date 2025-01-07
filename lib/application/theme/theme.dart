d16b19dadcd73abba88d8fc38fb0fb0f1075f2e1
cch-robo <robo@cch-lab.com>
2025-01-07 12:47:49 +0900

    TextTheme の残りプロパティタイプを追加

--- lib/application/theme/theme.dart
+++ lib/application/theme/theme.dart
@@ -32,7 +32,21 @@ TextTheme _createTextTheme(BuildContext context, ColorScheme colorScheme) {
   final ThemeData baseThemeData = Theme.of(context);
   final TextTheme baseTextTheme = baseThemeData.textTheme;
   return TextTheme(
+    displayLarge: _convertColor(baseTextTheme.displayLarge, colorScheme),
     displayMedium: _convertColor(baseTextTheme.displayMedium, colorScheme),
+    displaySmall: _convertColor(baseTextTheme.displaySmall, colorScheme),
+    headlineLarge: _convertColor(baseTextTheme.headlineLarge, colorScheme),
+    headlineMedium: _convertColor(baseTextTheme.headlineMedium, colorScheme),
+    headlineSmall: _convertColor(baseTextTheme.headlineSmall, colorScheme),
+    titleLarge: _convertColor(baseTextTheme.titleLarge, colorScheme),
+    titleMedium: _convertColor(baseTextTheme.titleMedium, colorScheme),
+    titleSmall: _convertColor(baseTextTheme.titleSmall, colorScheme),
+    bodyLarge: _convertColor(baseTextTheme.bodyLarge, colorScheme),
+    bodyMedium: _convertColor(baseTextTheme.bodyMedium, colorScheme),
+    bodySmall: _convertColor(baseTextTheme.bodySmall, colorScheme),
+    labelLarge: _convertColor(baseTextTheme.labelLarge, colorScheme),
+    labelMedium: _convertColor(baseTextTheme.labelMedium, colorScheme),
+    labelSmall: _convertColor(baseTextTheme.labelSmall, colorScheme),
   );
 }
 
