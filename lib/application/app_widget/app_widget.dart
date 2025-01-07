c51adf74b6a8b64b72ee736cc8915910f2b25fbe
cch-robo <robo@cch-lab.com>
2025-01-07 19:16:23 +0900

    生成された delegate を localizationsDelegates に追加 - AppLocalizations.delegate

--- lib/application/app_widget/app_widget.dart
+++ lib/application/app_widget/app_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:search_repositories_on_github/application/l10n/gen/app_localizations.dart';
 import 'package:search_repositories_on_github/application/theme/theme.dart';
 import 'package:search_repositories_on_github/presentation/home_page/page_widget/home_page_widget.dart';
 
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
       theme: createThemeData(context),
       darkTheme: createDarkThemeData(context),
       localizationsDelegates: [
+        AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
