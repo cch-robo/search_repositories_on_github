5a1444fe7458b3d533acdce44c5362180461851b
cch-robo <robo@cch-lab.com>
2025-01-07 18:43:45 +0900

    国際化対応の設定とリソースと生成ファイル配置用ディレクトリを新規追加 - app_widget.dart: 国際化対応設定を追加 - l10n.yaml: 国際化対応クラス生成設定ファイル - /l10n: 国際化リソース配置用ディレクトリ   - app_ja.arb: 日本語リソース   - app_en.arb: 英語リソース - /l10n/gen: 生成クラス配置用ディレクトリ

--- lib/application/app_widget/app_widget.dart
+++ lib/application/app_widget/app_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:search_repositories_on_github/application/theme/theme.dart';
 import 'package:search_repositories_on_github/presentation/home_page/page_widget/home_page_widget.dart';
 
@@ -11,6 +12,15 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: createThemeData(context),
       darkTheme: createDarkThemeData(context),
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('en'),
+        Locale('jp'),
+      ],
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
