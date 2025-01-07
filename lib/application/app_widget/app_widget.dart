982f9c6f60408531b31be83fb1889f193e63af76
cch-robo <robo@cch-lab.com>
2025-01-08 08:41:28 +0900

    エラーダイアログ等の表示には、グローバルコンテキストが必要なので追加

--- lib/application/app_widget/app_widget.dart
+++ lib/application/app_widget/app_widget.dart
@@ -3,6 +3,18 @@ import 'package:search_repositories_on_github/application/l10n/gen/app_localizat
 import 'package:search_repositories_on_github/application/theme/theme.dart';
 import 'package:search_repositories_on_github/presentation/home_page/page_widget/home_page_widget.dart';
 
+/// グローバルにアクセス可能な Navigator のキー
+///
+GlobalKey<NavigatorState> globalNavigatorKey =
+    GlobalKey<NavigatorState>(debugLabel: 'app');
+
+/// グローバルにアクセス可能な Navigator 稼働中フラグ
+bool get isGlobalNavigatorRunning => globalNavigatorKey.currentState != null;
+
+/// グローバルにアクセスが可能な Navigator の [BuildContext]
+BuildContext get globalNavigatorContext =>
+    globalNavigatorKey.currentState!.context;
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -10,6 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      navigatorKey: globalNavigatorKey,
       theme: createThemeData(context),
       darkTheme: createDarkThemeData(context),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
