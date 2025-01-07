28a53bf943667148fd96ff8d80372fee87cd1423
cch-robo <robo@cch-lab.com>
2025-01-07 12:24:30 +0900

    TextTheme 文字色が背景色と適合するよう ColorScheme の背景反転色の利用に改訂

--- lib/application/theme/theme.dart
+++ lib/application/theme/theme.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 ThemeData createThemeData(BuildContext context) {
   final ColorScheme colorScheme = _createColorScheme(isDark: false);
-  final TextTheme textTheme = _createTextTheme(context);
+  final TextTheme textTheme = _createTextTheme(context, colorScheme);
   return ThemeData.from(
     colorScheme: colorScheme,
     textTheme: textTheme,
@@ -12,7 +12,7 @@ ThemeData createThemeData(BuildContext context) {
 
 ThemeData createDarkThemeData(BuildContext context) {
   final ColorScheme colorScheme = _createColorScheme(isDark: true);
-  final TextTheme textTheme = _createTextTheme(context);
+  final TextTheme textTheme = _createTextTheme(context, colorScheme);
   return ThemeData.from(
     colorScheme: colorScheme,
     textTheme: textTheme,
@@ -28,12 +28,21 @@ ColorScheme _createColorScheme({required bool isDark}) {
   return colorScheme;
 }
 
-TextTheme _createTextTheme(BuildContext context) {
+TextTheme _createTextTheme(BuildContext context, ColorScheme colorScheme) {
   final ThemeData baseThemeData = Theme.of(context);
   final TextTheme baseTextTheme = baseThemeData.textTheme;
   return TextTheme(
-    displayMedium: baseTextTheme.displayMedium,
+    displayMedium: _convertColor(baseTextTheme.displayMedium, colorScheme),
   );
+}
+
+// ダークテーマ対応
+TextStyle? _convertColor(TextStyle? textStyle, ColorScheme colorScheme) {
+  if (textStyle == null) {
+    return null;
+  }
+  final Color color = colorScheme.onSurface;
+  return textStyle.copyWith(color: color);
 }
 
 const Color _seedColor = Color(0xFF2979FF);
