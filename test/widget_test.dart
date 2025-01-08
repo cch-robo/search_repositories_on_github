22e54bf8b4fe6bb42d4d37803272820d8dd7acfe
cch-robo <robo@cch-lab.com>
2025-01-08 16:53:45 +0900

    アプリクラス名修正

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:search_repositories_on_github/application/app_widget/app_widget.
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const App());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
