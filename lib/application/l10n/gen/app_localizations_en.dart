a94a08f9229318efe4c7f0c7b2b92e4e8727195f
cch-robo <robo@cch-lab.com>
2025-01-08 10:45:10 +0900

    エラー表示用の国際化リソースを追加

--- lib/application/l10n/gen/app_localizations_en.dart
+++ lib/application/l10n/gen/app_localizations_en.dart
@@ -12,4 +12,17 @@ class AppLocalizationsEn extends AppLocalizations {
 
   @override
   String get appTitle => 'Search Repo';
+
+  @override
+  String get errorDialogTitle => 'Error';
+
+  @override
+  String get errorDialogUnexpectedErrorMessage =>
+      'An unexpected error has occurred.\n\nWe are unable to fix the problem so please close the app.';
+
+  @override
+  String get errorButtonOk => 'OK';
+
+  @override
+  String get errorButtonCancel => 'Cancel';
 }
