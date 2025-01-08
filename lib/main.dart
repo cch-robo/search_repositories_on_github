a18023c65c345ea6e140355c1e10de29a80c8d54
cch-robo <robo@cch-lab.com>
2025-01-08 11:16:00 +0900

    アプリレベルのエラーハンドラにアプリ起動処理をバインド

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:search_repositories_on_github/application/app_widget/app_widget.dart';
+import 'package:search_repositories_on_github/application/error/app_error_dialog.dart';
+import 'package:search_repositories_on_github/application/error/app_error_handler.dart';
 
 void main() {
-  runApp(const MyApp());
+  // アプリレベルのエラーハンドラ＆エラーダイアログ・サービス
+  final AppErrorHandler appErrorHandler = AppErrorHandler();
+  final AppErrorHandlerDialog appErrorDialog = AppErrorHandlerDialog();
+
+  // アプリ起動（アプリ全体のエラーハンドリング指定含む）
+  appErrorHandler.runAppWithErrorHandler(
+    appErrorDialog: appErrorDialog,
+    appWidget: const MyApp(),
+    appInitialize: () async {
+      // Firebase ライブラリの設定初期化などに利用します。
+    },
+    onFlutterError: (FlutterErrorDetails details) {
+      // Firebase Crashlytics クラッシュログ送信などに利用します。
+    },
+    onPlatformError: (Object exception, StackTrace stackTrace) {
+      // Firebase Crashlytics クラッシュログ送信などに利用します。
+      return false;
+    },
+    onAsynchronousError: (Object error, StackTrace stackTrace) {
+      // Firebase Crashlytics クラッシュログ送信などに利用します。
+    },
+  );
 }
