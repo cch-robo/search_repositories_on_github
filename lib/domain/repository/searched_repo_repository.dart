f152c583e9914574d8ec2cd1e57c22a65a77ec05
cch-robo <robo@cch-lab.com>
2025-01-10 07:00:53 +0900

    同一 domainレイヤのクラスの import を相対パスにして明示化。

--- lib/domain/repository/searched_repo_repository.dart
+++ lib/domain/repository/searched_repo_repository.dart
@@ -1,8 +1,9 @@
 import 'package:dio/dio.dart';
-import 'package:search_repositories_on_github/domain/models/search_filers.dart';
-import 'package:search_repositories_on_github/domain/models/search_repositories_info_model.dart';
-import 'package:search_repositories_on_github/domain/rest_api/rest_api_service.dart';
 import 'package:search_repositories_on_github/foundation/publications.dart';
+
+import '../models/search_filers.dart';
+import '../models/search_repositories_info_model.dart';
+import '../rest_api/rest_api_service.dart';
 
 class SearchedRepoRepository {
   const SearchedRepoRepository(this._restApiService);
