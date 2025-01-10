a20fe3e51063d74f8176bd002c3e777b0e433cdd
cch-robo <robo@cch-lab.com>
2025-01-11 07:55:26 +0900

    画面構成を riverpod 対応に修正

--- lib/presentation/search_page/page_widget/search_page_widget.dart
+++ lib/presentation/search_page/page_widget/search_page_widget.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:search_repositories_on_github/application/publications.dart';
 import 'package:search_repositories_on_github/foundation/publications.dart';
+import 'package:search_repositories_on_github/presentation/search_page/view_model/search_page_view_model.dart';
 import 'package:search_repositories_on_github/presentation/ui_components/app_icon.dart';
 import 'package:search_repositories_on_github/presentation/ui_components/simple_button.dart';
 
+import '../../ui_components/progress_panel.dart';
 import '../../ui_components/simple_text.dart';
 import '../../ui_components/simple_textfield.dart';
+
+part 'search_page_widget.g.dart';
+
+@immutable
+class CounterState {
+  const CounterState(this.count);
+
+  final int count;
+}
+
+@riverpod
+class CounterViewModel extends _$CounterViewModel {
+  CounterViewModel();
+
+  @override
+  CounterState build() {
+    return const CounterState(0);
+  }
+
+  void increment() {
+    state = CounterState(state.count + 1);
+  }
+}
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -19,7 +45,14 @@ class SearchPageState extends State<SearchPage> {
   final TextEditingController _readmeController = TextEditingController();
   final TextEditingController _descriptionController = TextEditingController();
   final TextEditingController _repoNameController = TextEditingController();
-  final TextEditingController _repoOwnerController = TextEditingController();
+  final TextEditingController _topicsController = TextEditingController();
+
+  void clear() {
+    _readmeController.clear();
+    _descriptionController.clear();
+    _repoNameController.clear();
+    _topicsController.clear();
+  }
 
   @override
   void initState() {
@@ -32,82 +65,99 @@ class SearchPageState extends State<SearchPage> {
     _readmeController.dispose();
     _descriptionController.dispose();
     _repoNameController.dispose();
-    _repoOwnerController.dispose();
+    _topicsController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     debugLog('debug - SearchPage - build');
-    return Scaffold(
-      appBar: AppBar(
-        leading: const AppIcon(),
-        title: Text(l10n(context).appTitle),
-      ),
-      body: SafeArea(
-        top: true,
-        bottom: true,
-        left: true,
-        right: true,
-        child: Consumer(
-          builder: (BuildContext context, WidgetRef ref, Widget? child) {
-            return Column(
-              children: <Widget>[
-                // スクロール可能なコンテンツ
-                Expanded(
-                  child: SingleChildScrollView(
-                    child: Center(
-                      child: Column(
-                        children: <Widget>[
-                          const SimpleText('GitHub リポジトリ検索'),
-                          const SimpleText(
-                            '検索条件を指定してください。',
-                            size: FontSize.large,
+    return Stack(
+      children: <Widget>[
+        Scaffold(
+          appBar: AppBar(
+            leading: const AppIcon(),
+            title: Text(l10n(context).appTitle),
+          ),
+          body: SafeArea(
+            top: true,
+            bottom: true,
+            left: true,
+            right: true,
+            child: Consumer(
+              builder: (BuildContext context, WidgetRef ref, Widget? child) {
+                final SearchPageViewModel viewModel =
+                    ref.watch(searchPageViewModelProvider.notifier);
+
+                return Column(
+                  children: <Widget>[
+                    // スクロール可能なコンテンツ
+                    Expanded(
+                      child: SingleChildScrollView(
+                        child: Center(
+                          child: Column(
+                            children: <Widget>[
+                              const SimpleText('GitHub リポジトリ検索'),
+                              const SimpleText(
+                                '検索条件を指定してください。',
+                                size: FontSize.large,
+                              ),
+                              const SizedBox(height: 20),
+                              const SimpleText('README 条件'),
+                              SimpleTextField(
+                                labelText: 'README に含まれるテキストを入力してください',
+                                controller: _readmeController,
+                              ),
+                              const SizedBox(height: 20),
+                              const SimpleText('description 条件'),
+                              SimpleTextField(
+                                labelText: 'description に含まれるテキストを入力してください',
+                                controller: _descriptionController,
+                              ),
+                              const SizedBox(height: 20),
+                              const SimpleText('リポジトリ名 条件'),
+                              SimpleTextField(
+                                labelText: 'リポジトリ名 に含まれるテキストを入力してください',
+                                controller: _repoNameController,
+                              ),
+                              const SizedBox(height: 20),
+                              const SimpleText('topics 条件'),
+                              SimpleTextField(
+                                labelText: 'topics のラベルを入力してください',
+                                controller: _topicsController,
+                              ),
+                            ],
                           ),
-                          const SizedBox(height: 20),
-                          const SimpleText('README 条件'),
-                          SimpleTextField(
-                            labelText: 'README に含まれるテキストを入力してください',
-                            controller: _readmeController,
-                          ),
-                          const SizedBox(height: 20),
-                          const SimpleText('description 条件'),
-                          SimpleTextField(
-                            labelText: 'description に含まれるテキストを入力してください',
-                            controller: _descriptionController,
-                          ),
-                          const SizedBox(height: 20),
-                          const SimpleText('リポジトリ名 条件'),
-                          SimpleTextField(
-                            labelText: 'リポジトリ名 に含まれるテキストを入力してください',
-                            controller: _repoNameController,
-                          ),
-                          const SizedBox(height: 20),
-                          const SimpleText('オーナー名 条件'),
-                          SimpleTextField(
-                            labelText: 'リポジトリオーナの名前を入力してください',
-                            controller: _repoOwnerController,
-                          ),
-                        ],
+                        ),
                       ),
                     ),
-                  ),
-                ),
-                // 固定ボタン
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: SimpleButton(
-                    label: '検索',
-                    onPressed: (BuildContext context) {
-                      ResultsPageRoute().go(context);
-                    },
-                  ),
-                ),
-              ],
-            );
-          },
+                    // 【検索】ボタン
+                    Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: SimpleButton(
+                        label: '検索',
+                        onPressed: (BuildContext context) async {
+                          await viewModel.search(
+                            readme: _readmeController.text,
+                            description: _descriptionController.text,
+                            repoName: _repoNameController.text,
+                            topics: _topicsController.text,
+                          );
+                          if (viewModel.isComplete && context.mounted) {
+                            ResultsPageRoute().go(context);
+                          }
+                        },
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
         ),
-      ),
+        // 検索実行中のプログレス表示用
+        const ProgressPanel(type: ProgressPageType.search),
+      ],
     );
   }
 }
