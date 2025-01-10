dd08f01622e22f20083e97c64dc16c0b26892975
cch-robo <robo@cch-lab.com>
2025-01-11 07:12:54 +0900

    リポジトリ名のみを表示する検索一覧用のカードウィジェットを新規追加

---
+++ lib/presentation/ui_components/repository_card.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+// リポジトリ名、オーナーアイコン、プロジェクト言語、Star 数、Watcher 数、Fork 数、Issue 数
+
+/// リポジトリの名前のみを表示するカードです
+class RepositoryCard extends StatelessWidget {
+  const RepositoryCard({
+    required void Function(BuildContext context, int id) onPressed,
+    required int id,
+    required String name,
+    super.key,
+  })  : _onPressed = onPressed,
+        _id = id,
+        _name = name;
+
+  /// リポジトリID
+  final int _id;
+
+  /// リポジトリ名
+  final String _name;
+
+  final void Function(BuildContext context, int id) _onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        child: GestureDetector(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                leading: const Icon(Icons.album),
+                title: Text(_name),
+              ),
+            ],
+          ),
+          onTap: () => _onPressed(context, _id),
+        ),
+      ),
+    );
+  }
+}
