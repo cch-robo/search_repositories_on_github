fbde614c2d42767deb2bb5a5d998fa513fdb5432
cch-robo <robo@cch-lab.com>
2025-01-11 12:32:19 +0900

    import 構成を整理してレイヤごとの依存関係を明示＋ライブラリ名を修正 - 検索APIから取得したデータを表示するように修正。

--- lib/presentation/results_page/page_widget/results_page_widget.dart
+++ lib/presentation/results_page/page_widget/results_page_widget.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:search_repositories_on_github/presentation/ui_components/progress_panel.dart';
-import 'package:search_repositories_on_github/presentation/ui_components/repository_card.dart';
+import 'package:search_repositories_on_github/application/publications.dart';
+import 'package:search_repositories_on_github/domain/publications.dart';
+import 'package:search_repositories_on_github/foundation/publications.dart';
 
-import '../../../application/router/routes.dart';
-import '../../../foundation/publications.dart';
+import '../../detail_page/view_model/detail_page_view_model.dart';
+import '../../ui_components/progress_panel.dart';
+import '../../ui_components/repository_card.dart';
+import '../view_model/results_page_view_model.dart';
 
 class ResultsPage extends HookConsumerWidget {
   const ResultsPage({super.key});
@@ -23,11 +26,9 @@ class ResultsPage extends HookConsumerWidget {
     debugLog('debug - ResultsPage - build');
     useEffect(_initState, <Object?>[]);
 
-    //FIXME プロバイダもしくは検索サービスからの取得に修正すること。
-    final List<int> items = List<int>.generate(
-      50,
-      (int index) => index,
-    );
+    final ResultsPageViewModel viewModel =
+        ref.read(resultsPageViewModelProvider.notifier);
+
     return Stack(
       children: <Widget>[
         Scaffold(
@@ -44,13 +45,20 @@ class ResultsPage extends HookConsumerWidget {
                 SliverList(
                   delegate: SliverChildBuilderDelegate(
                     (BuildContext context, int index) {
-                      return (index >= items.length)
+                      final RepoModel? repo = viewModel.getRepoInfo(index);
+                      return repo == null
                           ? null
                           : RepositoryCard(
-                              id: index,
-                              name: 'index:$index',
-                              onPressed: (BuildContext context, int id) {
+                              index: index,
+                              name: repo.name,
+                              onPressed: (BuildContext context, int index) {
+                                // カードがタップされたら、DetailPage で詳細を表示する。
                                 DetailPageRoute().go(context);
+
+                                // タップされたリポジトリ情報の index を通知する。
+                                final DetailPageViewModel viewModel = ref
+                                    .read(detailPageViewModelProvider.notifier);
+                                viewModel.notifyShowDetail(index: index);
                               },
                             );
                     },
