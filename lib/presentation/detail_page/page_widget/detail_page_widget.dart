ba163c31cd365aa4dd8350f0944355d5a0f019fe
cch-robo <robo@cch-lab.com>
2025-01-11 16:37:54 +0900

    DetailPageにパラメータを追加 - index: リポジトリ情報の index

--- lib/presentation/detail_page/page_widget/detail_page_widget.dart
+++ lib/presentation/detail_page/page_widget/detail_page_widget.dart
@@ -1,13 +1,15 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:search_repositories_on_github/domain/publications.dart';
 
-import '../../ui_components/simple_button.dart';
+import '../../ui_components/simple_text.dart';
 import '../view_model/detail_page_view_model.dart';
 
 class DetailPage extends HookConsumerWidget {
-  const DetailPage({super.key});
+  const DetailPage({required this.index, super.key});
+  final int index;
 
   Dispose? _initState() {
     return _dispose;
@@ -16,6 +18,7 @@ class DetailPage extends HookConsumerWidget {
   Dispose? _dispose() {
     return null;
   }
+//  リポジトリ名、オーナーアイコン、プロジェクト言語、Star 数、Watcher 数、Fork 数、Issue 数
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -24,6 +27,7 @@ class DetailPage extends HookConsumerWidget {
 
     final DetailPageViewModel viewModel =
         ref.read(detailPageViewModelProvider.notifier);
+    final RepoModel repo = viewModel.getRepoInfo(index)!;
 
     return Scaffold(
       appBar: AppBar(title: const Text('Detail Page')),
@@ -32,21 +36,70 @@ class DetailPage extends HookConsumerWidget {
         bottom: true,
         left: true,
         right: true,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.max,
-            children: <Widget>[
-              SimpleButton(
-                label: 'back to Results page',
-                onPressed: (BuildContext context) {
-                  context.pop();
-                },
+        child: Column(
+          children: <Widget>[
+            // スクロール可能なコンテンツ
+            Expanded(
+              child: SingleChildScrollView(
+                child: Center(
+                  child: Column(
+                    children: <Widget>[
+                      const SimpleText('GitHub リポジトリ検索'),
+                      const Divider(),
+                      ListTile(
+                        title: const SimpleText('リポジトリ名'),
+                        subtitle: SimpleText(repo.name, size: FontSize.medium),
+                      ),
+                      const Divider(),
+                      FittedBox(
+                        fit: BoxFit.contain,
+                        child: Image.network(repo.ownerAvatarUrl),
+                      ),
+                      const Divider(),
+                      ListTile(
+                        title: const SimpleText('プロジェクト言語'),
+                        subtitle:
+                            SimpleText(repo.language, size: FontSize.medium),
+                      ),
+                      const Divider(),
+                      ListTile(
+                        title: const SimpleText('Star 数'),
+                        subtitle: SimpleText(repo.startsCount.toString(),
+                            size: FontSize.medium),
+                      ),
+                      const Divider(),
+                      ListTile(
+                        title: const SimpleText('Watcher 数'),
+                        subtitle: SimpleText(repo.watchersCount.toString(),
+                            size: FontSize.medium),
+                      ),
+                      const Divider(),
+                      ListTile(
+                        title: const SimpleText('Fork 数'),
+                        subtitle: SimpleText(repo.forksCount.toString(),
+                            size: FontSize.medium),
+                      ),
+                      const Divider(),
+                      ListTile(
+                        title: const SimpleText('Issue 数'),
+                        subtitle: SimpleText(repo.issuesCount.toString(),
+                            size: FontSize.medium),
+                      ),
+                      const Divider(),
+                    ],
+                  ),
+                ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(IntProperty('index', index));
   }
 }
