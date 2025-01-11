513685762e3f460bcc3d73c78c1c4a5c093e1fb3
cch-robo <robo@cch-lab.com>
2025-01-11 12:34:18 +0900

    riverpod プロバイダーを利用するように修正

--- lib/presentation/detail_page/page_widget/detail_page_widget.dart
+++ lib/presentation/detail_page/page_widget/detail_page_widget.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../ui_components/simple_button.dart';
+import '../view_model/detail_page_view_model.dart';
 
 class DetailPage extends HookConsumerWidget {
   const DetailPage({super.key});
 
+  Dispose? _initState() {
+    return _dispose;
+  }
+
+  Dispose? _dispose() {
+    return null;
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     debugPrint('debug - DetailPage - build');
+    useEffect(_initState, <Object?>[]);
+
+    final DetailPageViewModel viewModel =
+        ref.read(detailPageViewModelProvider.notifier);
+
     return Scaffold(
       appBar: AppBar(title: const Text('Detail Page')),
       body: SafeArea(
