09c727c24cf352d89c38a73c6432ebecfe298458
cch-robo <robo@cch-lab.com>
2025-01-24 12:21:07 +0900

    プレゼンテーション用のエラーダイアログを新規追加

---
+++ lib/presentation/ui_components/error_dialog.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+/// プレゼンテーションのエラーダイアログ表示
+class PresentationErrorDialog {
+  Future<void> showErrorAlertDialog({
+    required BuildContext context,
+    required String title,
+    required String message,
+  }) async {
+    if (context.mounted) {
+      await showDialog<void>(
+        context: context,
+        useRootNavigator: true,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          // バックボタンでのダイアログ表示クローズを抑止します。
+          return PopScope(
+            canPop: false,
+            child: AlertDialog(
+              title: Text(title),
+              content: SingleChildScrollView(
+                child: Text(message),
+              ),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            ),
+          );
+        },
+      );
+    }
+  }
+}
